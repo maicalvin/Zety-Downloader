6b202c5ee32230543452f5c535a73915475064dc
Mai Calvin <mai.calvin@yahoo.com>
2019-07-18 15:47:53 +0800

    Update zety_downloader.rb
    Zety did an update on 4th June and there are some changes on the Zety

--- zety_downloader.rb
+++ zety_downloader.rb
@@ -1,7 +1,8 @@
 require 'wicked_pdf'
-# require 'chromedriver-helper'
 require 'selenium-webdriver'
 require 'io/console'
+require 'chromedriver-helper'
+require 'capybara'
 
 path = Dir.pwd+'/temp_index.html'
 File.write(path, File.read('./index.html'))
@@ -17,10 +18,16 @@ print 'Link to Zety resume: '
 preview_site = gets.chomp
 
 options = Selenium::WebDriver::Chrome::Options.new(args: ['headless'])
-# options = Selenium::WebDriver::Chrome::Options.new
+ #options = Selenium::WebDriver::Chrome::Options.new
 driver = Selenium::WebDriver.for(:chrome, options: options)
-driver.get('https://app.zety.com/login')
-driver.title == "Log In | Zety" ? (puts 'Logging in and generating PDF, please wait for ~15 seconds') : ()
+
+driver.get('https://app.zety.com/login?v=classic')
+
+driver.title == "Resume Builder Login | Zety" ? (puts 'Logging in and generating PDF, please wait for ~15 seconds') : ()
+classic = driver.find_element(:id,'showLoginForm')
+sleep(5)
+driver.execute_script("arguments[0].click();",classic)
+puts 'success'
 
 email = driver.find_element(name: 'email')
 password = driver.find_element(name: 'password')
@@ -39,10 +46,14 @@ lines[11] = content << $/
 File.open(path, 'w') { |f| f.write(lines.join) }
 
 pdf = WickedPdf.new.pdf_from_html_file(path)
-File.open('./new_resume.pdf', 'wb') do |file|
+File.open('./new_resume', 'wb') do |file|
   file << pdf
 end
 driver.quit
 
 File.delete('./temp_index.html')
 puts 'PDF complete'
+
+WickedPdf.config = {
+  :exe_path => Rails.root.join('bin', 'wkhtmltopdf-i386').to_s,
+}
